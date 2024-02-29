745060ca126d40845b25bb4df75eee40aaa5d9db
Kostya <rayjackson@live.ru>
2024-02-29 05:02:29 +0300

    Finished pages.

--- src/pages/[lang]/index.tsx
+++ src/pages/[lang]/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import { useMemo, useRef } from 'react';
 
 import { Locales } from 'constants/locales';
@@ -6,6 +7,7 @@ import { Discography } from 'sections/Discography';
 import { Hero } from 'sections/Hero';
 import { Release } from 'sections/Release';
 import { Album } from 'types';
+import { getTranslation } from 'utils/helpers/getTranslation';
 import { loadAllReleases } from 'utils/helpers/loadAllReleases';
 import { loadLocale } from 'utils/helpers/loadLocale';
 
@@ -18,12 +20,24 @@ type Props = {
   releases: Album[];
 };
 
-const Home = ({ releases }: Props): JSX.Element => {
+const Home = ({ lang, releases }: Props): JSX.Element => {
   const sectionRef = useRef<HTMLElement>(null);
   const latestAlbum = useMemo(() => releases[0], [releases]);
+  const title = getTranslation('title');
+  const description = getTranslation('pageDescription');
 
   return (
     <>
+      <Head>
+        <html lang={lang} />
+        <title>{title}</title>
+        <meta content={description} name="description" />
+        <meta content={description} name="og:description" />
+        <meta content={title} name="og:title" />
+        <meta content="website" name="og:type" />
+        <meta content="/images/me.jpeg" name="og:image" />
+      </Head>
+
       <Hero nextSectionRef={sectionRef} />
       <Release album={latestAlbum} ref={sectionRef} />
       <Discography albums={releases} />
--- src/pages/[lang]/release/[slug].tsx
+++ src/pages/[lang]/release/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { Locales } from 'constants/locales';
+import { Release } from 'sections/Release';
 import { Album } from 'types';
+import { getTranslation } from 'utils/helpers/getTranslation';
 import { loadAllReleases } from 'utils/helpers/loadAllReleases';
 import { loadLocale } from 'utils/helpers/loadLocale';
 
@@ -16,8 +19,27 @@ type Props = {
 };
 
 const ReleasePage = ({ lang, release }: Props): JSX.Element => {
-  console.log(release, lang);
-  return <main>Test</main>;
+  const pageTitle = `${release?.title} | MELLAMOKOSTYA`;
+  const pageDesc = getTranslation('releasePageDesc').replace(
+    '#',
+    release?.title ?? '',
+  );
+
+  return (
+    <>
+      <Head>
+        <html lang={lang} />
+        <title>{pageTitle}</title>
+        <meta content={pageTitle} name="og:title" />
+        <meta content={pageDesc} name="description" />
+        <meta content={pageDesc} name="og:description" />
+        <meta content={release?.art} name="og:image" />
+        <meta content="website" name="og:type" />
+      </Head>
+
+      {release && <Release album={release} hasBorder={false} />}
+    </>
+  );
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
