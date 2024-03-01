1fe3546875dfd1146fe72c49251f0ce4a7be4f8c
rayjackson13 <rayjackson@live.ru>
2024-03-01 13:42:00 +0300

    Fixed background image.

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,17 +1,16 @@
-import Image from 'next/image';
 import React from 'react';
 
 import styles from './Background.module.scss';
+
+import { Image } from 'components/Image';
 
 export const Background = (): JSX.Element => (
   <div className={styles.bg}>
     <Image
       alt=""
       className={styles.img}
-      height={1000}
-      sizes="(max-height: 767px) 100vw, 1000px"
+      sizes="(max-width: 767px) 700w, 1000px"
       src="/images/me.jpg"
-      width={1000}
     />
     <div className={styles.gradient} />
   </div>
