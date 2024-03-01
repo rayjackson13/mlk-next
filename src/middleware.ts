4ac994eed77b38b1995a5f633f4f0bfa5cf48657
rayjackson13 <rayjackson@live.ru>
2024-03-01 11:05:03 +0300

    Caching images on client.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,22 @@ import { NextRequest, NextResponse } from 'next/server';
 const PUBLIC_FILE = /\.(.*)$/;
 
 export async function middleware(req: NextRequest): Promise<unknown> {
-  const headers = new Headers(req.headers);
   const { pathname, locale, search } = req.nextUrl;
 
-  if (
-    pathname.startsWith('/_next') ||
-    pathname.includes('/api/') ||
-    PUBLIC_FILE.test(pathname)
-  ) {
+  if (pathname.startsWith('/_next') || pathname.includes('/api/')) {
     return;
   }
 
+  if (PUBLIC_FILE.test(pathname)) {
+    const headers = new Headers();
+    headers.set('Cache-Control', 'public, max-age=86400');
+    return NextResponse.next({
+      headers,
+    });
+  }
+
   if (locale !== 'default') {
+    const headers = new Headers(req.headers);
     headers.set('x-site-locale', locale);
     return NextResponse.next({
       request: {
