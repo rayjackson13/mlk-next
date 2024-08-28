3054639fef689860c0a1899956eef7b32d85ba94
Kostya <rayjackson@live.ru>
2024-08-28 22:20:33 +0300

    Added new single Pustota. Fixed image optimization for Windows platforms.

--- scripts/optimiseImages.mjs
+++ scripts/optimiseImages.mjs
@@ -3,13 +3,18 @@ import path from 'path';
 import fs from 'fs-extra';
 import sharp from 'sharp';
 
+const formatPath = (pathString) => pathString.replace(/\//g, path.sep);
+
 const paths = [
-  path.resolve(process.cwd(), 'public/images'),
-  path.resolve(process.cwd(), 'public/images/music'),
+  path.resolve(process.cwd(), formatPath('public/images')),
+  path.resolve(process.cwd(), formatPath('public/images/music')),
 ];
 const ImageSizes = [250, 400, 700, 1000];
 const ImageExtRegex = /.png|.jpg|.jpeg/;
-const outputDir = path.resolve(process.cwd(), 'public/images/_optimized');
+const outputDir = path.resolve(
+  process.cwd(),
+  formatPath('public/images/_optimized'),
+);
 
 const quality = 80;
 
@@ -28,7 +33,8 @@ fs.rmSync(outputDir, { recursive: true, force: true });
 fs.mkdirSync(outputDir);
 
 files.forEach((file) => {
-  const localPath = file.split('/images/')[1].replace(ImageExtRegex, '');
+  const imagesPath = formatPath('/images/');
+  const localPath = file.split(imagesPath)[1].replace(ImageExtRegex, '');
   const dirPath = path.resolve(outputDir, localPath);
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath, { recursive: true });
@@ -39,10 +45,10 @@ files.forEach((file) => {
       .resize(size)
       .toFormat('png')
       .png({ progressive: true, quality })
-      .toFile(`${dirPath}/${size}w.png`);
+      .toFile(formatPath(`${dirPath}/${size}w.png`));
     sharp(file)
       .resize(size)
       .toFormat('webp')
-      .toFile(`${dirPath}/${size}w.webp`);
+      .toFile(formatPath(`${dirPath}/${size}w.webp`));
   });
 });
---
+++ src/data/releases/16-otpusti.md
@@ -0,0 +1,12 @@
+---
+slug: "otpusti"
+date: "2024-06-01"
+title: "Отпусти"
+art: /images/music/otpusti.png
+type: "single"
+links:
+  - { service: "spotify", url: "https://open.spotify.com/track/046QBGwbmdPIdlB1K9wJZM?si=88b70cd88daf4603" }
+  - { service: "apple", url: "https://music.apple.com/ru/album/%D0%BE%D1%82%D0%BF%D1%83%D1%81%D1%82%D0%B8/1749062819" }
+  - { service: "youtube", url: "https://music.youtube.com/watch?v=2ssZn3pBYmk&si=XcaBza-cVtvuKy2b" }
+  - { service: "yandex", url: "https://music.yandex.ru/album/31465818/track/126571110" }
+---
