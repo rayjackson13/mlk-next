55b8e04a0cc2de605d2a5bac52558a63fb4d7bec
Kostya <rayjackson@live.ru>
2024-08-29 01:19:18 +0300

    Updated image optimization to track time.

--- scripts/optimiseImages.mjs
+++ scripts/optimiseImages.mjs
@@ -3,6 +3,9 @@ import path from 'path';
 import fs from 'fs-extra';
 import sharp from 'sharp';
 
+const startTime = performance.now();
+console.debug('   📷  Optimizing images:');
+
 const formatPath = (pathString) => pathString.replace(/\//g, path.sep);
 
 const paths = [
@@ -31,24 +34,38 @@ const getFilesArray = () => paths.map(getFilesFromDirectory).flat();
 const files = getFilesArray();
 fs.rmSync(outputDir, { recursive: true, force: true });
 fs.mkdirSync(outputDir);
+const promises = [];
 
 files.forEach((file) => {
   const imagesPath = formatPath('/images/');
-  const localPath = file.split(imagesPath)[1].replace(ImageExtRegex, '');
+  const fileName = file.split(imagesPath)[1];
+  console.debug(`   - ${fileName}`);
+  const localPath = fileName.replace(ImageExtRegex, '');
   const dirPath = path.resolve(outputDir, localPath);
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath, { recursive: true });
   }
 
   ImageSizes.forEach((size) => {
-    sharp(file)
-      .resize(size)
-      .toFormat('png')
-      .png({ progressive: true, quality })
-      .toFile(formatPath(`${dirPath}/${size}w.png`));
-    sharp(file)
-      .resize(size)
-      .toFormat('webp')
-      .toFile(formatPath(`${dirPath}/${size}w.webp`));
+    promises.push(
+      sharp(file)
+        .resize(size)
+        .toFormat('png')
+        .png({ progressive: true, quality })
+        .toFile(formatPath(`${dirPath}/${size}w.png`)),
+      sharp(file)
+        .resize(size)
+        .toFormat('webp')
+        .toFile(formatPath(`${dirPath}/${size}w.webp`)),
+    );
   });
+});
+
+Promise.all(promises).then(() => {
+  const finishTime = performance.now() - startTime;
+  console.debug(
+    '\n',
+    `  ✅  Image optimization finished in ${(finishTime / 1000).toFixed(2)}`,
+    '\n',
+  );
 });
